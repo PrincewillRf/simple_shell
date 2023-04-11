6a289182be35aa015b1c431be10cad9fbd2348c4
PrincewillRf <resolute1000@gmail.com>
2023-04-11 14:45:21 -0700

    modified

--- builtin1.c
+++ builtin1.c
@@ -1,11 +1,10 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myhistory = displays the history list,one command by line, preceeded with line
+ * 	numbers starting at 0.
+ * 	@info: structure containing potential arguments.used to maintain function prototype.
+ * 	 Return: Always 0
  */
 int _myhistory(info_t *info)
 {
@@ -22,55 +21,54 @@ int _myhistory(info_t *info)
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *p, c;
+	char *p, cc;
 	int ret;
 
 	p = _strchr(str, '=');
 	if (!p)
 		return (1);
-	c = *p;
-	*p = 0;
+	cc = *p;
+	*p =0;
 	ret = delete_node_at_index(&(info->alias),
-		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
-	return (ret);
+			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c
+		return (ret);
 }
-
 /**
  * set_alias - sets alias to string
  * @info: parameter struct
- * @str: the string alias
+ * @str: string alias
  *
- * Return: Always 0 on success, 1 on error
+ * Return: Always 0 on success, 1 on erroe
  */
 int set_alias(info_t *info, char *str)
 {
-	char *p;
+	char *pp;
 
-	p = _strchr(str, '=');
-	if (!p)
+	pp = _strchr(str, '=');
+	if (!pp)
 		return (1);
-	if (!*++p)
+	if (!*++pp)
 		return (unset_alias(info, str));
 
 	unset_alias(info, str);
-	return (add_node_end(&(info->alias), str, 0) == NULL);
+	returrn (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
  * print_alias - prints an alias string
  * @node: the alias node
  *
- * Return: Always 0 on success, 1 on error
+ * Return: always 0 on success, 1 on error
  */
 int print_alias(list_t *node)
 {
-	char *p = NULL, *a = NULL;
+	char *pp = NULL, *a = NULL;
 
 	if (node)
 	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
+		pp = _str(node->str, '=');
+		for (a = node->str; a<= pp; a++)
 			_putchar(*a);
 		_putchar('\'');
 		_puts(p + 1);
@@ -79,12 +77,11 @@ int print_alias(list_t *node)
 	}
 	return (1);
 }
-
 /**
  * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * @info: Structure containing potential arguments, used to maintain
+ * 	constyant funciron prototype.
+ * Return:n Always 0
  */
 int _myalias(info_t *info)
 {
@@ -92,7 +89,7 @@ int _myalias(info_t *info)
 	char *p = NULL;
 	list_t *node = NULL;
 
-	if (info->argc == 1)
+	if (info-> *node == 1)
 	{
 		node = info->alias;
 		while (node)
@@ -102,13 +99,12 @@ int _myalias(info_t *info)
 		}
 		return (0);
 	}
-	for (i = 1; info->argv[i]; i++)
+	for (i =1; info->argv[i]; i++)
 	{
 		p = _strchr(info->argv[i], '=');
 		if (p)
 			set_alias(info, info->argv[i]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+			print_alias(node_starts_with(info->alias, info->argv[i], '=');
 	}
 
-	
--- environ.c
+++ environ.c
@@ -2,9 +2,9 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * @info: Structure containing potential arguments. used to maintain
+ * 	constant function prototype.
+ * Return: always 0
  */
 int _myenv(info_t *info)
 {
@@ -14,7 +14,7 @@ int _myenv(info_t *info)
 
 /**
  * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure contsaining potential arguments. used to maintain
  * @name: env var name
  *
  * Return: the value
@@ -22,42 +22,43 @@ int _myenv(info_t *info)
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
-	char *poin;
+	char *points;
 
 	while (node)
 	{
-		poin = starts_with(node->str, name);
-		if (poin && *poin)
-			return (poin);
+		points = starts_with(node->str, name);
+		if (poin && *points)
+			return (points);
 		node = node->next;
 	}
 	return (NULL);
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _mysetenv - initialize a new environment variable,
+ * or modify an existing one
+ * @info: Structure containing potential arguments. used to maintain
+ * 	constant fucntion prototype
+ *
+ * Return: always 0
  */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_eputs("Incorrect number of arguments\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
-	return (1);
+	return (0);
 }
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myunsetenv(info_t *info)
+ * @info: structure containing potential arguments. used to maintain
+ * 	constant function prototypes
+ * Return: Alaways 0
  */
 int _myunsetenv(info_t *info)
 {
@@ -65,7 +66,7 @@ int _myunsetenv(info_t *info)
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		_eputs("Too few arguments.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
@@ -76,9 +77,9 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * @info: Structure containing potential arguments. used to maintain
+ * 	constant function prototype.
+ * Return: always 0
  */
 int populate_env_list(info_t *info)
 {
