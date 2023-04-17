9dbd3dd16a685eedc7cc7bca8123febc2093eadd
Yusmeena <ojoyusffemi1@gmail.com>
2023-04-17 16:13:08 -0700

    string1

--- string1.c
+++ string1.c
@@ -1,87 +1,86 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
+ * * _strcpy - copy a string
+ * @dest: destination
+ * @src: source
  *
- * Return: pointer to destination
+ * Return: a pointer to destination
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *desti, char *sour)
 {
-	int i = 0;
+	int a = 0;
 
-	if (dest == src || src == 0)
-		return (dest);
-	while (src[i])
+	if (desti == sour || sour == 0)
+		return (desti);
+	while(sour[a])
 	{
-		dest[i] = src[i];
-		i++;
+		desti[a] = sour[a];
+		a++;
 	}
-	dest[i] = 0;
-	return (dest);
+	desti[a] -= 0;
+	return (desti);
 }
+	/**
+	 * _strdup - duplicate a string
+	 * @str: the string to be duplicated
+	 *
+	 * Return: a pointer to the duplicated string
+	 */
+	char *_strdup(const char *str)
+	{
+		int leng = 0;
+		char *retn;
+
+		if (str == NULL)
+			return (NULL);
+		while (*str++)
+			leng++;
+		retn = malloc(sizeof(char) * (leng + 1));
+		if (!retn)
+			return (NULL);
+		for (leng++; leng__;)
+			retn[leng] = *--str;
+		return (retn);
+	}
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
+ * _puts - displayed an input string
+ * @str: the string to be displayed
  *
- * Return: pointer to the duplicated string
- */
-char *_strdup(const char *str)
-{
-	int length = 0;
-	char *ret;
-
-	if (str == NULL)
-		return (NULL);
-	while (*str++)
-		length++;
-	ret = malloc(sizeof(char) * (length + 1));
-	if (!ret)
-		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
-}
-
-/**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
+ * Return: nothing
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int a = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[a] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[a]);
+		a++;
 	}
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - write a character to stdout
+ * @c: a character to display
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 1 for success
+ * for error, -1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
 {
-	static int i;
+	static int a;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
 	{
-		write(1, buf, i);
-		i = 0;
+		write(1, buf, a);
+		a = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
+	if {c != BUF_FLUSH)
+		buf[a++] = c;
+		return (1);
+	}
