213c1b547136a17d72bb5518089e00c253510c03
Yusmeena <ojoyusffemi1@gmail.com>
2023-04-21 02:37:07 -0700

    main

--- main.c
+++ main.c
@@ -13,10 +13,9 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -27,7 +26,7 @@ int main(int ac, char **av)
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
+				_eputs(": 0: cannot open ");
 				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
