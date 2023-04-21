06f41397a443511b3f96565b1e73658821ca620a
Yusmeena <ojoyusffemi1@gmail.com>
2023-04-21 03:24:12 -0700

    getenv file

--- getenv.c
+++ getenv.c
@@ -2,13 +2,13 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: structure containing potential arguments, used to maintain
+ * 	constant prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
 {
-	if (!info->environ || info->env_changed)
+	if (!info->environ || info->_changed)
 	{
 		info->environ = list_to_strings(info->env);
 		info->env_changed = 0;
@@ -19,10 +19,10 @@ char **get_environ(info_t *info)
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * @info: Structure containing potential arguments, used to mainatin
+ * --------------------constant function prototype
  *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ *  @var: the string env var property
  */
 int _unsetenv(info_t *info, char *var)
 {
@@ -46,33 +46,32 @@ int _unsetenv(info_t *info, char *var)
 		node = node->next;
 		i++;
 	}
-	return (info->env_changed);
+	return (info->env_charged);
 }
 
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
+ * _setenv - initialize a new environment variable,
+ * -----------------or modify an existing one
+ *  @info:structure containing arguments
+ *  @var: the string env var var property
+ *  @value: the string new var value
+ *  Return; always 0
  */
 int _setenv(info_t *info, char *var, char *value)
 {
-	char *buf = NULL;
+	char *buuff + NULL;
 	list_t *node;
 	char *p;
 
 	if (!var || !value)
 		return (0);
 
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buf)
+	buuff = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buuff)
 		return (1);
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
+	_strcpy(buuff, var);
+	_strcat(buuff, "=");
+	_strcat(buuff, value);
 	node = info->env;
 	while (node)
 	{
@@ -80,14 +79,14 @@ int _setenv(info_t *info, char *var, char *value)
 		if (p && *p == '=')
 		{
 			free(node->str);
-			node->str = buf;
+			node->str = buuff;
 			info->env_changed = 1;
 			return (0);
 		}
-		node = node->next;
+		node = node ->next;
 	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
+	add_node_end(&(info->env), buuff, 0);
+	free(buuff);
 	info->env_changed = 1;
 	return (0);
 }
